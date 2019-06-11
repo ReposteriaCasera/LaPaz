774b51285a085b26d94acccd5b2bd4da78d94291
eldetulado <eldetulado@gmail.com>
2019-06-11 00:07:34 -0400

    pantalla index

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-alert('hola mundo')
+var phone = '59179507041'
+var text = 'Hola,%20requiero%20de%20sus%20servicios'
+var url = 'https://api.whatsapp.com/send?phone=' + phone + '&text=' + text
+
+var whatsapp = document.getElementById('msg-whatsapp')
+whatsapp.href = url
---
+++ js/menu.js
@@ -0,0 +1,12 @@
+var menuItem = document.getElementById('menu-items')
+var contLogo = document.getElementById('container-logo')
+
+function menuOpen() {
+    if (menuItem.style.display === "none") {
+        menuItem.style.display = "inherit";
+        contLogo.style.background = '#000'
+    } else {
+        menuItem.style.display = "none";
+        contLogo.style.background = 'none'
+    }
+}
